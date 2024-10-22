7a2760cf56eddfe9565366490f5505adee6f3b5c
aaaamer1 <aaamer1@ualberta.ca>
2024-10-22 17:36:37 -0600

    first test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,10 +46,12 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount(){
+
         return cities.size();
     }
 
     public void addCity(City city){
+
 
     }
 
